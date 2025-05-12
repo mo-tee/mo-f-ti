49b214623c9014fb78d304f4866ff673058278b3
junghongseop <a01082372487@gmail.com>
2025-05-12 20:23:40 +0900

    feat: 글로벌 스타일 파일 추가

---
+++ src/components/common/global.ts
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+input[type="checkbox"],
+input[type="radio"] {
+  accent-color: #1470ff;
+  cursor: pointer;
+}
+
+
+a {
+  display: inline-block;
+  text-decoration: none;
+  color: inherit;
+}
+
+label {
+  cursor: pointer;
+}
+
+input,
+textarea {
+  -moz-user-select: auto;
+  -webkit-user-select: auto;
+  -ms-user-select: auto;
+  user-select: auto;
+  border: none;
+  outline: none;
+}
+
+input:focus {
+  outline: none;
+}
+
+button {
+  outline: none;
+  border: none;
+  background: none;
+  padding: 0;
+  cursor: pointer;
+}
+`;
+
+export default GlobalStyle;
