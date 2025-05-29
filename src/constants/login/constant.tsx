46f2519e50d1add1b7fa4b9f8cd906edbea3f75a
junghongseop <a01082372487@gmail.com>
2025-05-29 23:07:15 +0900

    feat: 로그인 리스트 추가

---
+++ src/constants/login/constant.tsx
@@ -0,0 +1,52 @@
+import { SlideData } from "@/types/login/client";
+
+export const SLIDE: SlideData[] = [
+  {
+    image: "/goalList.svg",
+    content: (
+      <>
+        모티에서 올바른
+        <br />
+        소비습관을 키워보세요
+      </>
+    ),
+    width: 305,
+    height: 321,
+  },
+  {
+    image: "/ai.svg",
+    content: (
+      <>
+        목표를 만들고
+        <br />
+        AI에게 분석받으세요
+      </>
+    ),
+    width: 291,
+    height: 362,
+  },
+  {
+    image: "/quiz.svg",
+    content: (
+      <>
+        매일 바뀌는 금융 상식 퀴즈로
+        <br />
+        지식과 포인트를 얻어보세요
+      </>
+    ),
+    width: 299,
+    height: 288.97,
+  },
+  {
+    image: "/itemList.svg",
+    content: (
+      <>
+        포인트를 통해
+        <br />
+        원하는 상품을 구매하세요
+      </>
+    ),
+    width: 303,
+    height: 337,
+  },
+];
