56ca7f4cf3c98c5a55f057109cff1ff008ce2a71
junghongseop <a01082372487@gmail.com>
2025-06-15 23:53:24 +0900

    feat: 상점에서 보여주는 포인트 컴포넌트 퍼블리싱

---
+++ src/components/shop/Point/Point.tsx
@@ -0,0 +1,41 @@
+import { Text } from "@/components/common";
+import Row from "@/components/common/Flex/Row";
+import { color } from "@/components/desgin-system";
+import { IconCoin } from "@/components/icon";
+import { flex } from "@/utils";
+import styled from "styled-components";
+
+interface PointProps {
+  point: string;
+}
+
+const Point = ({ point }: PointProps) => {
+  return (
+    <StyledPoint>
+      <Text fontType="Label2" color={color.G900}>
+        내 포인트
+      </Text>
+      <Row gap={4} alignItems="center">
+        <IconCoin width={20} height={20} />
+        <PointText>{point}P</PointText>
+      </Row>
+    </StyledPoint>
+  );
+};
+
+export default Point;
+
+const StyledPoint = styled.div`
+  ${flex({ flexDirection: "column", alignItems: "flex-end" })}
+  width: 100%;
+  gap: 2px;
+`;
+
+const PointText = styled.div`
+  color: ${color.G900};
+  font-family: KIMM_Bold;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 140%;
+`;
