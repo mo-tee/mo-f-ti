974f0f07ab30da73bcf92c219297ec14a111f0fb
junghongseop <a01082372487@gmail.com>
2025-06-15 23:55:14 +0900

    feat: 상점 아이템 상세보기 페이지 개발

---
+++ src/app/shop/[id]/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { color } from "@/components/desgin-system";
+import { IconBackArrow } from "@/components/icon";
+import { BuyButton, DetailContent } from "@/components/shop";
+import { flex } from "@/utils";
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
+
+const ShopDetail = () => {
+  const router = useRouter();
+
+  const handleMoveBack = () => {
+    router.back();
+  };
+
+  return (
+    <StyledShopDetail>
+      <IconWrapper>
+        <div onClick={handleMoveBack}>
+          <IconBackArrow width={24} height={24} />
+        </div>
+      </IconWrapper>
+      <DetailContentScroll>
+        <DetailContent />
+      </DetailContentScroll>
+      <BuyButton />
+    </StyledShopDetail>
+  );
+};
+
+export default ShopDetail;
+
+const StyledShopDetail = styled.div`
+  ${flex({
+    flexDirection: "column",
+    alignItems: "center",
+  })}
+  width: 100vw;
+  height: 100vh;
+  padding: 70px 16px 109px;
+  margin: 0 auto;
+  background-color: ${color.G10};
+  gap: 22px;
+
+  @media (min-width: 601px) {
+    width: 375px;
+  }
+`;
+
+const IconWrapper = styled.div`
+  ${flex({ justifyContent: "flex-start", alignItems: "center" })}
+  width: 100%;
+`;
+
+const DetailContentScroll = styled.div`
+  flex: 1 1 auto;
+  width: calc(100% + 32px);
+  margin-left: -16px;
+  margin-right: -16px;
+  min-height: 0;
+  overflow-y: auto;
+
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
