7a144a2deacfa3447ea057d8ca71a176f8c788e0
junghongseop <a01082372487@gmail.com>
2025-06-16 14:16:14 +0900

    feat: 성공한 리스트 컴포넌트 개발

---
+++ src/components/home/SuccessList/SuccessList.tsx
@@ -0,0 +1,48 @@
+import { Text } from "@/components/common";
+import Row from "@/components/common/Flex/Row";
+import { color } from "@/components/desgin-system";
+import { ROUTES } from "@/constants/common/constant";
+import { flex } from "@/utils";
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
+import GoalItem from "../GoalItem/GoalItem";
+import { DUMMY_SUCCESS_GOAL } from "@/constants/home/mock";
+
+const SuccessList = () => {
+  const router = useRouter();
+
+  const handlePlusClick = () => {
+    router.push(ROUTES.SUCCESS);
+  };
+
+  return (
+    <StyledSuccessList>
+      <Row width="100%" alignItems="center" justifyContent="space-between">
+        <Text fontType="Title3" color={color.G900}>
+          성공한 목표
+        </Text>
+        <div onClick={handlePlusClick}>
+          <Text fontType="Body2" color={color.G80}>
+            더보기
+          </Text>
+        </div>
+      </Row>
+      {DUMMY_SUCCESS_GOAL.slice(0, 3).map((goal) => (
+        <GoalItem
+          key={goal.id}
+          id={goal.id}
+          name={goal.name}
+          date={goal.date}
+        />
+      ))}
+    </StyledSuccessList>
+  );
+};
+
+export default SuccessList;
+
+const StyledSuccessList = styled.div`
+  ${flex({ flexDirection: "column", alignItems: "center" })}
+  width: 100%;
+  gap: 14px;
+`;
