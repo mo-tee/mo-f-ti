cc0493a4d3bb970cdc8f520c3b5756e99ef7b340
junghongseop <a01082372487@gmail.com>
2025-06-16 14:11:13 +0900

    fix: router 추가

--- src/constants/common/constant.ts
+++ src/constants/common/constant.ts
@@ -1,8 +1,10 @@
 export const ROUTES = {
   LOGIN: "/",
   HOME: "/home",
+  PROGRESS: "/home/progress",
+  SUCCESS: "/home/success",
   PROFILE: "/profile",
   QUIZ: "/quiz",
   SHOP: "/shop",
-  GOAL: "/goal"
+  GOAL: "/goal",
 };
