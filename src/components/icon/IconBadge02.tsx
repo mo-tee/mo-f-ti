066b8906e27b41b9c6adc21132881dfbc9b1b40f
junghongseop <a01082372487@gmail.com>
2025-06-16 14:15:56 +0900

    add: 뱃지 아이콘 1, 2 추가

---
+++ src/components/icon/IconBadge02.tsx
@@ -0,0 +1,394 @@
+import { SVGProps } from "react";
+
+const IconBadge02 = ({ ...props }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 216 221"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_538_3011)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M84 108H132V205.009C132 207.272 129.591 208.72 127.593 207.659L108 197.25L88.4075 207.659C86.4093 208.72 84 207.272 84 205.009V108Z"
+          fill="#265388"
+        />
+      </g>
+      <circle
+        style={{ mixBlendMode: "multiply" }}
+        opacity="0.2"
+        cx="108"
+        cy="114"
+        r="60"
+        fill="#62CF3F"
+      />
+      <circle cx="108" cy="108" r="60" fill="url(#paint0_linear_538_3011)" />
+      <mask
+        id="mask0_538_3011"
+        style={{ maskType: "luminance" }}
+        maskUnits="userSpaceOnUse"
+        x="48"
+        y="48"
+        width="120"
+        height="120"
+      >
+        <circle cx="108" cy="108" r="60" fill="white" />
+      </mask>
+      <g mask="url(#mask0_538_3011)">
+        <path
+          d="M49.5 108C49.5 140.309 75.6913 166.5 108 166.5C140.309 166.5 166.5 140.309 166.5 108"
+          stroke="#64788F"
+          strokeWidth="3"
+        />
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M147 119.258L182.258 84"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M132 86.258L167.258 51"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M21 143.258L56.258 108"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M153 77.258L188.258 42"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M42 134.258L77.258 99"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M93 170.258L128.258 135"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M141 149.258L176.258 114"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <path
+          d="M149.366 149.366C159.952 138.779 166.5 124.154 166.5 108C166.5 75.6913 140.309 49.5 108 49.5C75.6913 49.5 49.5 75.6913 49.5 108C49.5 124.154 56.0478 138.779 66.6343 149.366"
+          stroke="#8B99AF"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.4"
+          d="M166.5 108C166.5 75.6913 140.309 49.5 108 49.5C75.6913 49.5 49.5 75.6913 49.5 108"
+          stroke="#CCD8EA"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M108 49.5C97.0254 49.5 86.7566 52.522 77.9804 57.7793"
+          stroke="white"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.6"
+          d="M97.5424 50.432C94.7564 50.9348 92.0391 51.6348 89.4053 52.5171"
+          stroke="white"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+      </g>
+      <g filter="url(#filter1_d_538_3011)">
+        <circle cx="108" cy="108" r="45" fill="url(#paint1_linear_538_3011)" />
+        <circle
+          cx="108"
+          cy="108"
+          r="46.5"
+          stroke="#64788F"
+          strokeOpacity="0.06"
+          strokeWidth="3"
+        />
+      </g>
+      <mask
+        id="mask1_538_3011"
+        style={{ maskType: "luminance" }}
+        maskUnits="userSpaceOnUse"
+        x="63"
+        y="63"
+        width="90"
+        height="90"
+      >
+        <circle cx="108" cy="108" r="45" fill="white" />
+      </mask>
+      <g mask="url(#mask1_538_3011)">
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M132 80.258L167.258 45"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M153 71.258L188.258 36"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M42 128.258L77.258 93"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M93 164.258L128.258 129"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <g style={{ mixBlendMode: "multiply" }} opacity="0.12">
+          <path
+            d="M147 113.258L182.258 78"
+            stroke="#78A2A9"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </g>
+        <path
+          opacity="0.6"
+          d="M108 64.5C102.102 64.5 96.4783 65.6738 91.3495 67.8005"
+          stroke="white"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+      </g>
+      <g filter="url(#filter2_d_538_3011)">
+        <path
+          d="M90.8006 108L108 85.8864L125.199 108L108 130.113L90.8006 108Z"
+          fill="white"
+        />
+        <path
+          d="M108 82.8864C108.926 82.8864 109.8 83.3138 110.368 84.0446L127.567 106.158C128.357 107.173 128.407 108.57 127.716 109.633L127.567 109.841L110.368 131.956C109.8 132.686 108.925 133.114 108 133.114C107.074 133.114 106.2 132.686 105.632 131.956L88.4324 109.841C87.59 108.758 87.5899 107.241 88.4324 106.158L105.632 84.0446L105.742 83.9108C106.31 83.2618 107.132 82.8864 108 82.8864Z"
+          stroke="white"
+          strokeWidth="6"
+          strokeLinejoin="round"
+        />
+      </g>
+      <g filter="url(#filter3_i_538_3011)">
+        <path
+          d="M90.8005 108L108 85.8864L125.199 108L108 130.113L90.8005 108Z"
+          fill="url(#paint2_linear_538_3011)"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_538_3011"
+          x="78"
+          y="108"
+          width="60"
+          height="112.013"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="6" />
+          <feGaussianBlur stdDeviation="3" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.603922 0 0 0 0 0.713726 0 0 0 0 0.356863 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="darken"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_538_3011"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_538_3011"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_538_3011"
+          x="48"
+          y="51"
+          width="120"
+          height="120"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="3" />
+          <feGaussianBlur stdDeviation="6" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.279884 0 0 0 0 0.409586 0 0 0 0 0.470833 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_538_3011"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_538_3011"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter2_d_538_3011"
+          x="78.8006"
+          y="76.8864"
+          width="58.3988"
+          height="68.2271"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="3" />
+          <feGaussianBlur stdDeviation="3" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.19669 0 0 0 0 0.27695 0 0 0 0 0.483333 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_538_3011"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_538_3011"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter3_i_538_3011"
+          x="90.8005"
+          y="85.8864"
+          width="34.3988"
+          height="47.2271"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="3" />
+          <feGaussianBlur stdDeviation="1.5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.324977 0 0 0 0 0.428027 0 0 0 0 0.579167 0 0 0 0.4 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_538_3011"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_538_3011"
+          x1="48"
+          y1="48"
+          x2="48"
+          y2="168"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#ABB7CC" />
+          <stop offset="1" stopColor="#76879C" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_538_3011"
+          x1="63"
+          y1="63"
+          x2="63"
+          y2="153"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#C6D1E4" />
+          <stop offset="1" stopColor="#A4B1C4" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_538_3011"
+          x1="90.8005"
+          y1="85.8864"
+          x2="90.8005"
+          y2="130.113"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#A3B0C5" />
+          <stop offset="1" stopColor="#7A8AA1" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default IconBadge02;
