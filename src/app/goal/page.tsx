b66d81b854e5b60e49b3355093defc376bfeb0a9
junghongseop <a01082372487@gmail.com>
2025-06-20 10:37:13 +0900

    feat: 목표 생성 페이지 개발

---
+++ src/app/goal/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Navigation, Text } from "@/components/common";
+import { color } from "@/components/desgin-system";
+import { GoalCreateContent } from "@/components/goal";
+import { flex } from "@/utils";
+import styled from "styled-components";
+
+const Goal = () => {
+  return (
+    <StyledGoal>
+      <Text fontType="Label1" color={color.G900}>
+        소비 목표 생성
+      </Text>
+      <ScrollContent>
+        <GoalCreateContent />
+      </ScrollContent>
+      <Navigation />
+    </StyledGoal>
+  );
+};
+
+export default Goal;
+
+const StyledGoal = styled.div`
+  ${flex({
+    flexDirection: "column",
+    alignItems: "center",
+  })}
+  width: 100vw;
+  height: 100vh;
+  background-color: ${color.G20};
+  margin: 0 auto;
+  padding: 70px 16px 109px;
+  gap: 14px;
+
+  @media (min-width: 601px) {
+    width: 375px;
+  }
+`;
+
+const ScrollContent = styled.div`
+  flex: 1 1 auto;
+  width: 100%;
+  min-height: 0;
+  overflow-y: auto;
+
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
