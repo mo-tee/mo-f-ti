b4ac629532eee88886a6ffe21e7d019574fc6288
junghongseop <a01082372487@gmail.com>
2025-06-30 09:55:11 +0900

    fix: 웹뷰에서 안되던 웹 폰트 수정

--- src/components/desgin-system/global.ts
+++ src/components/desgin-system/global.ts
@@ -2,10 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 @font-face {
-    font-family: 'Pretendard-Regular';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
+  font-family: "Pretendard";
+  src: url("/fonts/PretendardVariable.woff2") format("woff2");
+  font-weight: 45 920;
+  font-style: normal;
 }
 
 @font-face {
@@ -19,6 +19,10 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+}
+
+body {
+  font-family: "Pretendard", sans-serif;
 }
 
 input[type="checkbox"],
