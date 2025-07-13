baefcb5dff15611132cc5371cd32fc1e9ddd108b
junghongseop <a01082372487@gmail.com>
2025-07-14 02:31:56 +0900

    fix: 주석 삭제

--- src/services/user/queries.ts
+++ src/services/user/queries.ts
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { getUser } from "./api";
 import { KEY } from "@/constants/common/constant";
 
-// 적용 전
 export const useUserQuery = () => {
   const { data, ...restQuery } = useQuery({
     queryKey: [KEY.USER] as const,
