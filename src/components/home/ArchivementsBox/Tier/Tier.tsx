284c3107ebab3aea1d7553b89b95d99e125baf74
junghongseop <a01082372487@gmail.com>
2025-07-16 08:29:59 +0900

    fix: 티어 정해줌 레벨에 따라서

--- src/components/home/ArchivementsBox/Tier/Tier.tsx
+++ src/components/home/ArchivementsBox/Tier/Tier.tsx
@@ -1,6 +1,16 @@
 import Column from "@/components/common/Flex/Column";
 import { color } from "@/components/desgin-system";
-import { IconBadge01 } from "@/components/icon";
+import {
+  IconBadge01,
+  IconBadge09,
+  IconBadge07,
+  IconBadge06,
+  IconBadge05,
+  IconBadge04,
+  IconBadge03,
+  IconBadge08,
+  IconBadge02,
+} from "@/components/icon";
 import { flex } from "@/utils";
 import styled from "styled-components";
 
@@ -8,13 +18,27 @@ interface TierProps {
   level: number;
 }
 
+const tierList = [
+  { name: "브론즈", icon: IconBadge01 },
+  { name: "실버", icon: IconBadge02 },
+  { name: "골드", icon: IconBadge03 },
+  { name: "플래티넘", icon: IconBadge04 },
+  { name: "챌린저", icon: IconBadge05 },
+  { name: "마스터", icon: IconBadge06 },
+  { name: "다이아", icon: IconBadge07 },
+  { name: "루비", icon: IconBadge08 },
+  { name: "정복자", icon: IconBadge09 },
+];
+
 const Tier = ({ level }: TierProps) => {
+  const { name, icon: IconComponent } = tierList[level - 1] || tierList[0];
+
   return (
     <StyledTier>
-      <IconBadge01 width={64} height={64} />
+      <IconComponent width={64} height={64} />
       <Column gap={4} alignItems="flex-start">
         <LevelText>Lv.{level}</LevelText>
-        <TierText>브론즈</TierText>
+        <TierText>{name}</TierText>
       </Column>
     </StyledTier>
   );
