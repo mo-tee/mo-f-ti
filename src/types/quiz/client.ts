3007f25831b20933b24b03ff2be3301912e32a82
junghongseop <a01082372487@gmail.com>
2025-07-16 08:44:11 +0900

    feat: 목표및 퀴즈와 관련된 타입 추가

--- src/types/quiz/client.ts
+++ src/types/quiz/client.ts
@@ -1,1 +1,19 @@
 export type QuizStep = "오늘의문제" | "해결한문제";
+
+export interface DailyQuiz {
+  id: number;
+  question: string;
+  answers: Answer[];
+}
+
+export interface Answer {
+  answerNumber: number;
+  answer: string;
+}
+
+export interface DailyQuizList {
+  userAnswer: number;
+  correctAnswer: number;
+  quiz: DailyQuiz;
+  solved: boolean;
+}
